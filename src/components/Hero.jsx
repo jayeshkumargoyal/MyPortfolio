a0d6895546e6dc3a836db0054c9ea8ecf41cac7b
jayeshkumargoyal <kumarjayesh012@gmail.com>
2025-05-19 19:10:08 -0600

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,11 +16,11 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Hello, I'm <span className='text-[#915EFF]'>Jayesh</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            Software Developer | Educator <br className='sm:block hidden' />
+            Building with AI and ML
           </p>
         </div>
       </div>
